6bac62ad3c6f65ca54982bb2e1e9303a5bf78225
AndradeNarciso <andradegregorio277@gmail.com>
2026-09-21 22:17:16 +0200

    tools added

--- src/main/java/com/andrade/mcp_shopping/McpShoppingApplication.java
+++ src/main/java/com/andrade/mcp_shopping/McpShoppingApplication.java
@@ -1,13 +1,25 @@
 package com.andrade.mcp_shopping;
 
+import java.util.List;
+
+import org.springframework.ai.support.ToolCallbacks;
+import org.springframework.ai.tool.ToolCallback;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.andrade.mcp_shopping.service.ShoppingCart;
 
 @SpringBootApplication
 public class McpShoppingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(McpShoppingApplication.class, args);
+	}
+
+	@Bean
+	public List<ToolCallback> tools(ShoppingCart shoppingCard) {
+		return List.of(ToolCallbacks.from(shoppingCard));
 	}
 
 }
--- src/main/java/com/andrade/mcp_shopping/service/ShoppingCart.java
+++ src/main/java/com/andrade/mcp_shopping/service/ShoppingCart.java
@@ -1,8 +1,62 @@
 package com.andrade.mcp_shopping.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.ai.tool.annotation.Tool;
 import org.springframework.stereotype.Service;
 
-@Service 
+import com.andrade.mcp_shopping.dtos.ShoppingItem;
+
+@Service
 public class ShoppingCart {
-    
+
+    private final Map<String, ShoppingItem> ShoppingList = new ConcurrentHashMap<>();
+
+    @Tool(name = "addItem", description = "add an item to the shopping list or update its quantity. Specify the item name and quantity.")
+    public String additem(String name, Integer quantity) {
+        if (name == null || name.trim().isEmpty() || quantity <= 0) {
+            return "Name or quantity invalid";
+        }
+
+        ShoppingList.compute(name.toLowerCase(), (key, item) -> {
+            if (item != null) {
+                return new ShoppingItem(item.name(), item.quantity() + quantity);
+            }
+
+            return new ShoppingItem(key, quantity);
+        });
+        return quantity + " unit of " + name + " added";
+    }
+
+    @Tool(name = "getItem", description = "Get all items currently in the shopping list. Return a list of items their name ond quantities.")
+    public List<ShoppingItem> getItems() {
+
+        return new ArrayList<>(ShoppingList.values());
+    }
+
+    @Tool(name = "removeItem", description = "Remove a specific quantity of  an item from the shopping list, Specify item name and quantity to remove. If quantity is not specified or is greater  than item quantity the item is removed")
+    public String removeItem(String name, Integer quantity) {
+        if (name == null || name.trim().isEmpty()) {
+            return "Item: " + name + " was not found at Shopping list";
+        }
+
+        ShoppingItem item = ShoppingList.get(name.toLowerCase());
+
+        if (quantity <= 0 || quantity > item.quantity()) {
+            return "Invalid quantity";
+        }
+
+        ShoppingList.compute(name.toLowerCase(), (key, currentItem) -> {
+            if (currentItem.quantity() - quantity != 0) {
+                return new ShoppingItem(currentItem.name(), currentItem.quantity() - quantity);
+            }
+
+            return null;
+        });
+
+        return "Item: " + name + " removed successfully";
+    }
 }
